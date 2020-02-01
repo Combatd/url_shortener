02809d08195ea5505cf9a562a72ed472320f242e
Combatd <combatdstar@gmail.com>
2020-02-01 00:26:20 -0800

    Index each email in users table

--- db/migrate/20200130222449_create_users.rb
+++ db/migrate/20200130222449_create_users.rb
@@ -1,7 +1,14 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
+      t.email :string
       
+      t.timestamps
     end
+
+    add_index( :users,
+      :email,
+      { unique: true }
+    )    
   end
 end
