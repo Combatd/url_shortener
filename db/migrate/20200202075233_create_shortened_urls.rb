cbca095a98e7f17cc2e4a261893a940f17f745c1
Combatd <combatdstar@gmail.com>
2020-02-02 04:51:19 -0800

    Define CreateShortenedUrls migration with index on submit_user_id, :short_url (unique)

--- db/migrate/20200202075233_create_shortened_urls.rb
+++ db/migrate/20200202075233_create_shortened_urls.rb
@@ -1,8 +1,20 @@
 class CreateShortenedUrls < ActiveRecord::Migration[5.2]
   def change
     create_table :shortened_urls do |t|
-      
+      t.string :short_url, null: false
+      t.string :long_url, null: false
+      t.integer :submit_user_id, null: false
+
+      t.timestamps
     end
-  
+    
+  add_index( :shortened_urls,
+      :submit_user_id
+  )
+  add_index( :shortened_urls,
+      :short_url,
+      unique: true
+  )
+
   end
 end
