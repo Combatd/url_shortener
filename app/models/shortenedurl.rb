18d1352839ffffd4183e5911f29f524f98fbc622
Combatd <combatdstar@gmail.com>
2020-02-06 19:45:12 -0800

    validate long_url on ShortenedUrl model

--- app/models/shortenedurl.rb
+++ app/models/shortenedurl.rb
@@ -1,4 +1,6 @@
 class ShortenedUrl < ApplicationRecord
     validates :short_url, uniqueness: true
     validates :short_url, presence: true
+    validates :long_url, uniqueness: true
+    validates :long_url, presence: true
 end
