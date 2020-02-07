ddcaf0293dd8d4642d5203f33b4e13cd7c39ebca
Combatd <combatdstar@gmail.com>
2020-02-06 21:37:36 -0800

    Define ShortenedUrl::random_code

--- app/models/shortenedurl.rb
+++ app/models/shortenedurl.rb
@@ -3,4 +3,15 @@ class ShortenedUrl < ApplicationRecord
     validates :short_url, presence: true
     validates :long_url, uniqueness: true
     validates :long_url, presence: true
+
+    def self.random_code
+        new_url = SecureRandom.urlsafe_base64
+
+        until !ShortenedUrl.exists?(short_url: new_url)
+            new_url = SecureRandom.urlsafe_base64
+        end
+
+        return new_url
+    end
+    
 end
