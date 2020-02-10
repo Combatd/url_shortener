1481f75c51ee1efdded666b25f8ccdc8ca1233e6
Combatd <combatdstar@gmail.com>
2020-02-10 12:12:12 -0800

    Define Shortenedurl##num_clicks

--- app/models/shortenedurl.rb
+++ app/models/shortenedurl.rb
@@ -1,14 +1,22 @@
 class ShortenedUrl < ApplicationRecord
+    validates :submit_user_id, presence: true
     validates :short_url, uniqueness: true
     validates :short_url, presence: true
     validates :long_url, uniqueness: true
     validates :long_url, presence: true
 
-    belongs_to (:submitter,
+    belongs_to(:submitter,
         primary_key: :id,
         foreign_key: :submit_user_id,
         class_name: :User
     )
+
+    has_many( :visits, dependent: :destroy,
+        primary_key: :short_url,
+        foreign_key: :shortened_urlm
+        class_name: :Visit
+    )
+
 
     def self.random_code
         new_url = SecureRandom.urlsafe_base64
@@ -26,6 +34,10 @@ class ShortenedUrl < ApplicationRecord
            long_url: long_url, 
            short_url: ShortenedUrl.random_code
         )
+    end
+
+    def num_clicks
+        self.visits.count
     end
 
 end
