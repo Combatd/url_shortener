8743361f2beeed2e512c3c41509fe06480976d55
Combatd <combatdstar@gmail.com>
2020-02-10 11:19:15 -0800

    Define CreateVisits migration

--- db/migrate/20200210191512_create_visits.rb
+++ db/migrate/20200210191512_create_visits.rb
@@ -1,6 +1,14 @@
 class CreateVisits < ActiveRecord::Migration[5.2]
   def change
     create_table :visits do |t|
+      t.string :shortened_url, null: false
+      t.integer :visitor_id, null: false
+
+      t.timestamps
     end
+    
+    add_index :visits, :shortened_url
+    add_index :visits, :visitor_id
+
   end
 end
