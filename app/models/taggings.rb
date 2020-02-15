3aec76e0a556d8385c2e1f466e37dfa39be9ab92
Combatd <combatdstar@gmail.com>
2020-02-15 12:50:30 -0800

    ASsociate #Tagging belongs_to short_url

--- app/models/taggings.rb
+++ app/models/taggings.rb
@@ -1,4 +1,10 @@
 class Tagging < ApplicationRecord
     validates :url_id, :tag_topic_id, presence: true
     
+    belongs_to( :short_url,
+    primary_key: :id,
+    foreign_key: :url_id,
+    class_name: :ShortenedUrl
+    )
+    
 end
