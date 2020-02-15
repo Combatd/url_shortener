d4aa8b0856874ba09435f74a8527800e930b033b
Combatd <combatdstar@gmail.com>
2020-02-15 12:00:15 -0800

    Define migration #AddTagoTopics##change to create_table :tag_topcis

--- db/migrate/20200215194006_add_tag_topics.rb
+++ db/migrate/20200215194006_add_tag_topics.rb
@@ -1,4 +1,10 @@
 class AddTagTopics < ActiveRecord::Migration[5.2]
   def change
+    create_table :tag_topics do |t|
+      t.string :topic
+      t.integer :views
+
+      t.timestamps
+    end
   end
 end
