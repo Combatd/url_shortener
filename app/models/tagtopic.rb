ecbea588e88cbd4b863e62824d01c02a33e5d673
Combatd <combatdstar@gmail.com>
2020-02-15 13:28:50 -0800

    Associate #TagTopic has many taggings

--- app/models/tagtopic.rb
+++ app/models/tagtopic.rb
@@ -2,8 +2,15 @@ class TagTopic < ApplicationRecord
     validates :topic, uniqueness: true
     validates :topic, presence: true
 
-    has_many :short_urls, 
+    has_many(:taggings, 
+    primary_key: :id, 
+    foreign_key: :tag_topic_id, 
+    class_name: :Tagging
+    )
+
+    has_many(:short_urls, 
     through: :taggings, 
     source: :short_url
-
+    )
+    
 end
