36220bc52c34656138564ba5cb2f7e3013a307f1
Combatd <combatdstar@gmail.com>
2020-03-20 08:19:52 -0700

    Refactor class TagTopic and TagTopic#popular_links

--- app/models/tag_topic.rb
+++ app/models/tag_topic.rb
@@ -6,29 +6,22 @@
 #  updated_at :datetime         not null
 
 class TagTopic < ApplicationRecord
-    validates :topic, uniqueness: true
-    validates :topic, presence: true
-
-    has_many(:taggings, 
-    primary_key: :id, 
-    foreign_key: :tag_topic_id, 
-    class_name: :Tagging
-    )
+    has_many :taggings
 
     has_many(:short_urls, 
     through: :taggings, 
-    source: :short_url
+    source: :shortened_url
     )
    
     # TagTopic#popular_links that returns the 5 most visited links for that TagTopic along with the number of times each link has been clicked.
 
     def popular_links
-        short_urls.joins(visits),
-        group(id),
-        order('COUNT(visitors.visitor_id),
+        short_urls.joins(:visits)
+        .group(:id),
+        .order('COUNT(visitors.visitor_id),
             DESC'),
-        select('short_url, COUNT(visitors.visitor_id) AS number_of_visits'),
-        limit(5)
+        .select('short_url, COUNT(visitors.visitor_id) AS number_of_visits'),
+        .limit(5)
     end
 
 end
